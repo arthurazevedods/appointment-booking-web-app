21b654c652ad8d03bf72f6e8b09d172e82b80a97
arthur <arthurazevedods@gmail.com>
2025-02-19 20:56:17 -0300

    hero init

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h2>Page</h2>
-      <Button>Button</Button>
+      <Hero />
 
     </div>
   );
