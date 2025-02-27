d722ce8a2de97ad29b8b25f1b0edfbdb0b3aa300
arthur <arthurazevedods@gmail.com>
2025-02-27 00:39:42 -0300

    hero

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Hero() {
     return (
@@ -6,16 +7,19 @@ export default function Hero() {
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-16">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
                     <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-                        <img
-                            src="https://images.unsplash.com/photo-1731690415686-e68f78e2b5bd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            className="absolute inset-0 h-full w-full object-cover rounded"
+                        <Image
+                            src="/banner-hero.jpg"
+                            className="absolute inset-0 h-full w-full object-cover 
+                            rounded-3xl"
                             alt="banner"
+                            width={800}
+                            height={800}
                         />
                     </div>
 
                     <div className="sm:py-12 lg:py-24">
                         <h2 className="lg:text-5xl sm:text-2xl font-bold capitalize text-gray-900">
-                            Lorem ipsum dolor 
+                            Lorem ipsum dolor
                         </h2>
 
                         <p className="mt-4 text-gray-700 lg:text-lg">
@@ -24,7 +28,9 @@ export default function Hero() {
                             sequi.
                         </p>
 
-                        <Button className="mt-4 capitalize">Book your appointment!</Button>
+                        <Button className="mt-4 capitalize">
+                            Book your appointment!
+                        </Button>
                     </div>
                 </div>
             </div>
